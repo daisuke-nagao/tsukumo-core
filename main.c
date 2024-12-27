3dffa4592c575842e0f4c10aa3e5ad14e22defa4
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2024-12-27 17:34:33 +0900

    Add BSS section initialization process
    - Implemented the `clear_bss` function to initialize the BSS section.
    - Used external symbols `_bss_start` and `_bss_end` to locate the BSS range.
    - Ensured that all memory in the BSS section is set to 0 during initialization.
    
    This change prepares the BSS section for use, addressing uninitialized memory issues.

--- main.c
+++ main.c
@@ -8,4 +8,10 @@ void tkmc_start(int a0, int a1) {
   return;
 }
 
-static void clear_bss(void) {}
+static void clear_bss(void) {
+  extern int *_bss_start;
+  extern int *_bss_end;
+  for (int *ptr = _bss_start; ptr < _bss_end; ++ptr) {
+    *ptr = 0;
+  }
+}
