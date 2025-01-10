2665329d481cc9256ee953fa1a990ae12dd48022
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-01-11 01:43:11 +0900

    Refactor task1 to use a helper function for string output
    - Introduced `putstring` to handle sending null-terminated strings to UART0.
    - Simplified `task1` implementation by replacing character-by-character output with a call to `putstring`.
    
    This refactoring improves code readability and reusability for UART-based string output.

--- task1.c
+++ task1.c
@@ -6,21 +6,15 @@
 
 volatile unsigned int *const UART0_BASE = (unsigned int *)0x10000000;
 
+static void putstring(const char *str) {
+  while (*str != '\0') {
+    *UART0_BASE = *str;
+    ++str;
+  }
+}
+
 void task1(void) {
-  *UART0_BASE = 'H';
-  *UART0_BASE = 'e';
-  *UART0_BASE = 'l';
-  *UART0_BASE = 'l';
-  *UART0_BASE = 'o';
-  *UART0_BASE = ',';
-  *UART0_BASE = ' ';
-  *UART0_BASE = 'w';
-  *UART0_BASE = 'o';
-  *UART0_BASE = 'r';
-  *UART0_BASE = 'l';
-  *UART0_BASE = 'd';
-  *UART0_BASE = '.';
-  *UART0_BASE = '\n';
+  putstring("Hello, world.\n");
   while (1) {
   }
 }
