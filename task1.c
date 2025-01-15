113d582a451f76973ffe6281dd761c2e03953e50
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-01-15 23:52:16 +0900

    Add task-to-task context switching in task1 and task2
    - Included `typedef.h` in `task1.c` and `task2.c` for type definitions.
    - Added calls to `tkmc_context_switch` in both `task1` and `task2` to enable context switching between tasks.
    - Updated `task1` to repeatedly output "Hello, world." and switch to `task2`.
    - Updated `task2` to repeatedly output "FizzBuzz." and switch back to `task1`.
    
    This update demonstrates basic task-to-task context switching functionality within the RTOS.

--- task1.c
+++ task1.c
@@ -5,9 +5,14 @@
  */
 
 #include "putstring.h"
+#include <typedef.h>
+
+extern void tkmc_context_switch(ID tskid);
 
 void task1(void) {
-  putstring("Hello, world.\n");
+  putstring("Hello, world\n");
   while (1) {
+    putstring("Hello, world.\n");
+    tkmc_context_switch(1);
   }
 }
--- task2.c
+++ task2.c
@@ -5,9 +5,14 @@
  */
 
 #include "putstring.h"
+#include <typedef.h>
+
+extern void tkmc_context_switch(ID tskid);
 
 void task2(void) {
   putstring("FizzBuzz\n");
   while (1) {
+    putstring("FizzBuzz.\n");
+    tkmc_context_switch(0);
   }
 }
