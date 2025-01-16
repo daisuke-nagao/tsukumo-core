829e546ba58a7c52e3849fc6fe741b2d86dcfd0b
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-01-17 00:15:55 +0900

    Add initial tkernel.h header for uT-Kernel 3.0 structure
    - Created `tkernel.h` in the `include/tk` directory as a placeholder for uT-Kernel 3.0-specific definitions.
    - Added a UUID-based include guard to prevent multiple inclusions.
    
    This update sets up a framework for integrating uT-Kernel 3.0-compliant headers.

---
+++ include/tk/tkernel.h
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Daisuke Nagao
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef UUID_01946FAC_8E45_7658_B009_C10ED747A05C
+#define UUID_01946FAC_8E45_7658_B009_C10ED747A05C
+
+#endif /* UUID_01946FAC_8E45_7658_B009_C10ED747A05C */
