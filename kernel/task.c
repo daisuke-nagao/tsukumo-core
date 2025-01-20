b1247946fb5d1b1f5379dcb01a564e0eaa102864
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-01-21 00:30:36 +0900

    Add initial files for task management
    - Included SPDX license headers and UUID-based include guard for `task.h`.
    
    This update establishes a dedicated module for task management, preparing for further functionality implementation.

---
+++ kernel/task.c
@@ -0,0 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Daisuke Nagao
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "task.h"
---
+++ kernel/task.h
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Daisuke Nagao
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef UUID_01948452_C50C_7B25_A131_C2733EF5E067
+#define UUID_01948452_C50C_7B25_A131_C2733EF5E067
+
+#endif /* UUID_01948452_C50C_7B25_A131_C2733EF5E067 */
