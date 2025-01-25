9e866b12c973f5863aa56f3a280b5ec85ca31997
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-01-26 01:34:01 +0900

    Add a free list header to Task Control Block (TCB) structure
    - Introduced `tkmc_free_tcb` as the head of the free TCB doubly linked list in `task.c`.
    - Initialized `tkmc_free_tcb` to point to itself in `tkmc_init_tcb`.
    - This change lays the groundwork for dynamic management of TCBs using a doubly linked list.

--- kernel/task.c
+++ kernel/task.c
@@ -7,9 +7,12 @@
 #include "task.h"
 
 TCB tkmc_tcbs[CFN_MAX_TSKID];
+TCB tkmc_free_tcb;
 TCB *current = NULL;
 
 void tkmc_init_tcb(void) {
+  tkmc_free_tcb.next = tkmc_free_tcb.prev = &tkmc_free_tcb;
+
   for (int i = 0; i < sizeof(tkmc_tcbs) / sizeof(tkmc_tcbs[0]); ++i) {
     TCB *tcb = &tkmc_tcbs[i];
     *tcb = (TCB){
