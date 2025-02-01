24d89f0907fdc9d60c5473c83b8c4ac9139f6b4a
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-02-02 01:00:21 +0900

    Add list.h for custom linked list implementation
    - Created `list.h` with basic structure to support linked list operations.
    - Included necessary header guards and SPDX license information.
    - Added C++ compatibility using `extern "C"`.
    - Prepared the file for future implementation of `list_head` and `container_of` macros.

---
+++ kernel/list.h
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Daisuke Nagao
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef UUID_0194C23B_B68F_7643_AA69_13C344A3D04F
+#define UUID_0194C23B_B68F_7643_AA69_13C344A3D04F
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* UUID_0194C23B_B68F_7643_AA69_13C344A3D04F */
