0911d8aa9abc11fafef883914a1f92a4fb6e6621
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-02-07 00:55:07 +0900

    RISC-V: Experimental interrupt and exception handling setup
    - Set `mtvec` to `mtrap_handler` for machine mode trap handling.
    - Enabled machine software interrupts (MSIP) by modifying `mie` and `mstatus`.
    - Implemented `mtrap_handler` to clear MSIP upon entry and return via `mret`.
    
    This implementation is for learning purposes and will be significantly revised later.

--- kernel/asm/rv32/start.s
+++ kernel/asm/rv32/start.s
@@ -10,8 +10,28 @@ _start:
   .option norelax
   la gp, __global_pointer$
   .option relax
+
+  la a0, mtrap_handler
+  csrw mtvec, a0
+
+  li a0, (1<<3)
+  csrs mie, a0
+  li a0, (1<<3)
+  csrs mstatus, a0
+
+  li a0, 0x2000000
+  li a1, 1
+  sw a1, 0(a0)
+
   la sp, _stack_end
   call tkmc_start
 1:
   wfi
   j 1b
+
+mtrap_handler:
+  // Clear MSIP
+  li a0, 0x2000000
+  li a1, 0
+  sw a1, 0(a0)
+  mret
