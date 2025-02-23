ef5ec931bb89765114308f4f3870c524f1c2f22a
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-02-24 00:01:47 +0900

    Add comprehensive I/O helper functions
    - Created `include/sys/ioport.h` as a placeholder for future I/O helper function definitions.
    - Established a structured header file with protection against multiple inclusion.
    - Included C++ extern "C" guards for compatibility with C++ compilers.

---
+++ include/sys/ioport.h
@@ -0,0 +1,12 @@
+#ifndef UUID_01953352_90E9_7F9F_ABE3_31D32A8E0817
+#define UUID_01953352_90E9_7F9F_ABE3_31D32A8E0817
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* UUID_01953352_90E9_7F9F_ABE3_31D32A8E0817 */
