8425bda84888b567ba8e26a48aed33aee1206f47
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-02-27 00:49:04 +0900

    Implement DI and EI macros for interrupt control
    - Defined `DI(intsts)` macro to disable interrupts and store previous status.
    - Defined `EI(intsts)` macro to restore interrupt status.
    - Integrated `sys/interrupt.h` into `tk/tkernel.h` for broader availability.

--- include/sys/interrupt.h
+++ include/sys/interrupt.h
@@ -1,9 +1,32 @@
 #ifndef UUID_019542DF_8F10_70F0_AE6B_848A71C04DB1
 #define UUID_019542DF_8F10_70F0_AE6B_848A71C04DB1
 
+#include <tk/typedef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
+
+#define MSTATUS_MIE ((UINT)(1 << 3))
+
+#define DI(intsts)                                                             \
+  do {                                                                         \
+    UINT status = 0;                                                           \
+    asm volatile("csrrci %0, mstatus, %1"                                      \
+                 : "=r"(status)                                                \
+                 : "i"(MSTATUS_MIE)                                            \
+                 : "memory");                                                  \
+    intsts = status | ~MSTATUS_MIE;                                            \
+  } while (0)
+
+#define EI(intsts)                                                             \
+  do {                                                                         \
+    if (intsts == 0) {                                                         \
+      asm volatile("csrsi mstatus, %0" ::"i"(MSTATUS_MIE) :);                  \
+    } else {                                                                   \
+      asm volatile("csrs mstatus, %0" ::"r"(intsts & MSTATUS_MIE) :);          \
+    }                                                                          \
+  } while (0)
 
 #ifdef __cplusplus
 } /* extern "C" */
