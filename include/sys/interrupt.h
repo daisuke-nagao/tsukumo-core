795980a21858a66d255dc23c7f02519ab63cb33c
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-02-27 00:29:22 +0900

    Add interrupt control header file
    - Created `sys/interrupt.h` for defining interrupt control functions.
    - Placeholder for future implementation of `DI()` and `EI()` functions.

---
+++ include/sys/interrupt.h
@@ -0,0 +1,12 @@
+#ifndef UUID_019542DF_8F10_70F0_AE6B_848A71C04DB1
+#define UUID_019542DF_8F10_70F0_AE6B_848A71C04DB1
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* UUID_019542DF_8F10_70F0_AE6B_848A71C04DB1 */
