97eb94cc9e49a44bb1b4019bdadd04e0730ad1e1
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-03-17 15:28:16 +0900

    [Feature] Add task2 and update usermain for task management
    - Added `task2.c` with a separate task implementation.
    - Updated `usermain.c` to manage task IDs using `get_tskid` for better task identification.
    - Modified `task1.c` to interact with `task2` using `tk_rel_wai` and `tk_wup_tsk`.
    - Adjusted stack sizes for tasks in `usermain.c`.
    - Enabled `-fstack-usage` flag in `CMakeLists.txt` for stack analysis.
    - Updated `linker.ld` to mark `.bss` and `.stack` sections as `NOLOAD`.

--- kernel/usermain.c
+++ kernel/usermain.c
@@ -7,18 +7,37 @@
 #include <tk/tkernel.h>
 
 extern void task1(INT stacd, void *exinf);
+extern void task2(INT stacd, void *exinf);
 
-static UW task1_stack[1024] __attribute__((aligned(16)));
-static UW task2_stack[1024] __attribute__((aligned(16)));
+static UW task1_stack[256] __attribute__((aligned(16)));
+static UW task2_stack[256] __attribute__((aligned(16)));
 
-static const char hello_world[] = "Hello, world.";
-static const char fizzbuzz[] = "FizzBuzz.";
+static ID s_id_map[2] = {
+    0,
+    0,
+};
+
+enum TASK_INDEX {
+  TASK1 = 0,
+  TASK2 = 1,
+  TASK_NBOF,
+};
+
+ID get_tskid(unsigned int index) {
+  if (index >= TASK_NBOF) {
+    return E_PAR;
+  }
+  return s_id_map[index];
+}
+
+static const char task1_exinf[] = "Task1";
+static const char task2_exinf[] = "Task2";
 
 void usermain(int _a0) __attribute__((weak));
 void usermain(int _a0) {
 
   T_CTSK pk_ctsk1 = {
-      .exinf = (void *)hello_world,
+      .exinf = (void *)task1_exinf,
       .tskatr = TA_USERBUF,
       .task = (FP)task1,
       .itskpri = 2,
@@ -27,9 +46,9 @@ void usermain(int _a0) {
   };
 
   T_CTSK pk_ctsk2 = {
-      .exinf = (void *)fizzbuzz,
+      .exinf = (void *)task2_exinf,
       .tskatr = TA_USERBUF,
-      .task = (FP)task1,
+      .task = (FP)task2,
       .itskpri = 2,
       .stksz = sizeof(task2_stack),
       .bufptr = task2_stack,
@@ -37,6 +56,9 @@ void usermain(int _a0) {
   ID task1_id = tk_cre_tsk(&pk_ctsk1);
   ID task2_id = tk_cre_tsk(&pk_ctsk2);
 
-  tk_sta_tsk(task1_id, task1_id);
-  tk_sta_tsk(task2_id, task2_id);
+  s_id_map[TASK1] = task1_id;
+  s_id_map[TASK2] = task2_id;
+
+  tk_sta_tsk(task1_id, 0);
+  tk_sta_tsk(task2_id, 0);
 }
