4c2631d1fc10a54665079fc88313318e679113fe
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-04-01 00:20:47 +0900

    [Add] Create semaphore.h header for semaphore support
    - Added `semaphore.h` with include guards and SPDX license info
    - Prepared for future semaphore-related kernel implementations
    - Currently includes only `<tk/tkernel.h>` and extern "C" block

---
+++ kernel/semaphore.h
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Daisuke Nagao
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef UUID_0195ECC8_7D68_710E_9F8F_8F0218E85157
+#define UUID_0195ECC8_7D68_710E_9F8F_8F0218E85157
+
+#include <tk/tkernel.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* UUID_0195ECC8_7D68_710E_9F8F_8F0218E85157 */
+
