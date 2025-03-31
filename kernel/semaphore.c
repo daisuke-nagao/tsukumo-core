b8296573fcfa59d04dab705634290e39dd41f34a
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-04-01 00:36:17 +0900

    [Add] Initialise semaphore control blocks and free list
    - Implemented `tkmc_init_semcb` to initialise all `SEMCB` entries
    - Marked each `SEMCB` as non-existent using `NOEXS_MASK`
    - Initialised `wait_queue` for each semaphore
    - Added all semaphores to the `tkmc_free_semcb` free list

--- kernel/semaphore.c
+++ kernel/semaphore.c
@@ -9,5 +9,22 @@
 SEMCB tkmc_semcbs[CFN_MAX_SEMID];
 static tkmc_list_head tkmc_free_semcb;
 
+// Bitmask used to mark control blocks as non-existent
+#define NOEXS_MASK 0x80000000u
+
 void tkmc_init_semcb(void) {
+  tkmc_init_list_head(&tkmc_free_semcb);
+
+  for (int i = 0; i < CFN_MAX_SEMID; ++i) {
+    SEMCB *semcb = &tkmc_semcbs[i];
+    *semcb = (SEMCB){
+        .semid = (i + 1) | NOEXS_MASK,
+        .exinf = NULL,
+        .sematr = 0,
+        .semcnt = 0,
+        .maxsem = 0,
+    };
+    tkmc_init_list_head(&semcb->wait_queue);
+    tkmc_list_add_tail(&semcb->wait_queue, &tkmc_free_semcb);
+  }
 }
