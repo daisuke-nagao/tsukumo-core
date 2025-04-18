dd1da34ac9ef50e64931d90742811829816d6b01
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-04-18 13:09:37 +0900

    feat: add mailbox functionality
    - Added mailbox.c and mailbox.h files to implement mailbox functionality.
    - Updated CMakeLists.txt to include mailbox.c in the kernel library.

---
+++ kernel/mailbox.c
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Daisuke Nagao
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "mailbox.h"
+
+#include <tk/tkernel.h>
---
+++ kernel/mailbox.h
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Daisuke Nagao
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef UUID_01964715_58B1_791F_8654_6C9F0D281D36
+#define UUID_01964715_58B1_791F_8654_6C9F0D281D36
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* UUID_01964715_58B1_791F_8654_6C9F0D281D36 */
