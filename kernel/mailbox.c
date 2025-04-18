e1f8b5b3a2776492dd8bb9c2c9b9ba6c1ef4d3dd
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2025-04-18 17:14:30 +0900

    feat: implement mailbox control block initialization
    - Defined CFN_MAX_MBXID in config.h to set the maximum number of mailboxes.
    - Added mailbox control block structure (MBXCB) in mailbox.h with attributes for wait queue, mailbox ID, extended information, and mailbox attributes.
    - Implemented tkmc_init_mbxcb function in mailbox.c to initialize mailbox control blocks and their wait queues.

--- kernel/mailbox.c
+++ kernel/mailbox.c
@@ -9,11 +9,30 @@
 
 #include <tk/tkernel.h>
 
-typedef struct MBXCB {
-} MBXCB;
+// Array of mailbox control blocks
+MBXCB tkmc_mbxcbs[CFN_MAX_MBXID];
+// Free list that holds mailbox control blocks that are not yet allocated
+static tkmc_list_head tkmc_free_mbxcb;
 
-ID tk_cre_mbx(const T_CMBX *pk_cmbx) {
-  // foo
+// Bitmask used to mark control blocks as non-existent (i.e. not allocated)
+#define NOEXS_MASK 0x80000000u
 
-  return E_LIMIT;
+void tkmc_init_mbxcb(void) {
+  tkmc_init_list_head(&tkmc_free_mbxcb);
+
+  for (int i = 0; i < CFN_MAX_MBXID; ++i) {
+    MBXCB *mbxcb = &tkmc_mbxcbs[i];
+    *mbxcb = (MBXCB){
+        // Set mbxid to (i+1) and mark as non-existent initially.
+        .mbxid = (i + 1) | NOEXS_MASK,
+        .exinf = NULL, // Extended information (user data), initially NULL.
+        .mbxatr = 0,   // Mailbox attributes; will be set on creation.
+    };
+    // Initialize the wait queue list head for each mailbox.
+    tkmc_init_list_head(&mbxcb->wait_queue);
+    // Add the mailbox control block's wait queue to the free list.
+    tkmc_list_add_tail(&mbxcb->wait_queue, &tkmc_free_mbxcb);
+  }
 }
+
+ID tk_cre_mbx(CONST T_CMBX *pk_cmbx) { return E_LIMIT; }
--- kernel/mailbox.h
+++ kernel/mailbox.h
@@ -7,9 +7,22 @@
 #ifndef UUID_01964715_58B1_791F_8654_6C9F0D281D36
 #define UUID_01964715_58B1_791F_8654_6C9F0D281D36
 
+#include <tk/tkernel.h>
+
+#include "list.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
+
+typedef struct MBXCB {
+  tkmc_list_head wait_queue; // Wait queue
+  ID mbxid;                  // Mailbox ID
+  void *exinf;               // Extended information
+  ATR mbxatr;                // Mailbox attributes
+} MBXCB;
+
+extern void tkmc_init_mbxcb(void);
 
 #ifdef __cplusplus
 } /* extern "C" */
